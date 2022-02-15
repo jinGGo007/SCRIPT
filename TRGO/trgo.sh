f447d0add8f4959c78548ace588463e1bad9e29f
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-02-15 18:46:10 +0800

    Add files via upload

--- TRGO/trgo.sh
+++ TRGO/trgo.sh
@@ -8,32 +8,37 @@ domain=$(cat /etc/v2ray/domain)
 # Uuid Service
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
-# Trojan Go Akun 
-mkdir -p /etc/trojan-go/
+# Install Trojan Go
+latest_version="$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/v${latest_version}/trojan-go-linux-amd64.zip"
+mkdir -p "/usr/bin/trojan-go"
+mkdir -p "/etc/trojan-go"
+cd `mktemp -d`
+curl -sL "${trojango_link}" -o trojan-go.zip
+unzip -q trojan-go.zip && rm -rf trojan-go.zip
+mv trojan-go /usr/local/bin/trojan-go
+chmod +x /usr/local/bin/trojan-go
+mkdir /var/log/trojan-go/
 touch /etc/trojan-go/akun.conf
-touch /etc/trojan-go/uuid.txt
-
-# Installing Trojan Go
-mkdir -p /etc/trojan-go/
-chmod 777 /etc/trojan-go/
 touch /etc/trojan-go/trojan-go.pid
-wget -O /etc/trojan-go/trojan-go https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/TRGO/trojan-go
-wget -O /etc/trojan-go/geoip.dat https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/TRGO/geoip.dat
-wget -O /etc/trojan-go/geosite.dat https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/TRGO/geosite.dat
-chmod +x /etc/trojan-go/trojan-go
-cat <<EOF > /etc/trojan-go/config.json
+touch /var/log/trojan-go/trojan-go.log
+
+
+# Buat Config Trojan Go
+cat > /etc/trojan-go/config.json << END
 {
-    "run_type": "server",
-    "local_addr": "0.0.0.0",
-    "local_port": 4443,
-    "remote_addr": "127.0.0.1",
-    "remote_port": 1421,
-    "log_level": 1,
-    "log_file": "/var/log/trojan-go.log",
-    "password": [
-        "$uuid"
-    ],
-  ""disable_http_check": true,
+  "run_type": "server",
+  "local_addr": "0.0.0.0",
+  "local_port": 4443,
+  "remote_addr": "127.0.0.1",
+  "remote_port": 1421,
+  "log_level": 1,
+  "log_file": "/var/log/trojan-go/trojan-go.log",
+ "password": [
+      "$uuid"
+  ],
+  },
+  "disable_http_check": true,
   "udp_timeout": 60,
   "ssl": {
     "verify": false,
@@ -53,34 +58,24 @@ cat <<EOF > /etc/trojan-go/config.json
     "plain_http_response": "",
     "fallback_addr": "127.0.0.1",
     "fallback_port": 0,
-    "fingerprint": ""
+    "fingerprint": "firefox"
   },
   "tcp": {
     "no_delay": true,
     "keep_alive": true,
-    "prefer_ipv4": true,
+    "prefer_ipv4": true
   },
   "mux": {
-    "enabled": true,
-    "concurrency": 64,
+    "enabled": false,
+    "concurrency": 8,
     "idle_timeout": 60
-  },
-  "router": {
-    "enabled": true,
-    "bypass": [],
-    "proxy": [],
-    "block": [],
-    "default_policy": "proxy",
-    "domain_strategy": "as_is",
-    "geoip": "/etc/trojan-go/geoip.dat",
-    "geosite": "/etc/trojan-go/geosite.dat"
   },
   "websocket": {
     "enabled": true,
-    "path": "/trgo",
+    "path": "/gandring",
     "host": "$domain"
   },
-  "api": {
+    "api": {
     "enabled": false,
     "api_addr": "",
     "api_port": 0,
@@ -93,48 +88,49 @@ cat <<EOF > /etc/trojan-go/config.json
     }
   }
 }
-EOF
-cat <<EOF > /etc/systemd/system/trojan-go.service
+END
+
+# Installing Trojan Go Service
+cat > /etc/systemd/system/trojan-go.service << END
 [Unit]
-Description=Trojan-Go 
-Documentation=https://p4gefau1t.github.io/trojan-go/
+Description=Trojan-Go Service By gandring
+Documentation=https://github.com/Gandring15/vps/main/
+Documentation=https://github.com/Gandring15/vps/main/
 After=network.target nss-lookup.target
+
 [Service]
-User=root
+Type=simple
+StandardError=journal
+PIDFile=/etc/trojan-go/trojan-go.pid
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/etc/trojan-go/trojan-go -config /etc/trojan-go/config.json
+ExecStart=/usr/local/bin/trojan-go -config /etc/trojan-go/config.json
+LimitNOFILE=65535
 Restart=on-failure
-RestartSec=10s
-LimitNOFILE=infinity
+RestartSec=1s
+
 [Install]
 WantedBy=multi-user.target
-EOF
+END
 
-cat <<EOF > /etc/trojan-go/uuid.txt
+# Trojan Go Uuid
+cat > /etc/trojan-go/uuid.txt << END
 $uuid
-EOF
+END
+
+# restart
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4443 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 4443 -j ACCEPT
-iptables-save >/etc/iptables.rules.v4
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save
 netfilter-persistent reload
 systemctl daemon-reload
-
-# Starting
-systemctl daemon-reload
-systemctl enable trojan-go.service
+systemctl stop trojan-go
 systemctl start trojan-go
+systemctl enable trojan-go
+systemctl restart trojan-go
 
-#tambahan
-figlet -f slant Install Module TROJAN-GO | lolcat
-wget -O /usr/bin/panel-trojan-go https://raw.githubusercontent.com/Dimas1441/ipvps/main/panel-trojan-go.sh && dos2unix /usr/bin/panel-trojan-go && chmod +x /usr/bin/panel-trojan-go
-wget -O /usr/bin/add-trgo https://raw.githubusercontent.com/Dimas1441/ipvps/main/add-trgo.sh && dos2unix /usr/bin/add-trgo && chmod +x /usr/bin/add-trgo
-wget -O /usr/bin/del-trgo https://raw.githubusercontent.com/Dimas1441/ipvps/main/del-trgo.sh && dos2unix /usr/bin/del-trgo && chmod +x /usr/bin/del-trgo
-wget -O /usr/bin/renew-trgo https://raw.githubusercontent.com/Dimas1441/ipvps/main/renew-trgo.sh && dos2unix /usr/bin/renew-trgo && chmod +x /usr/bin/renew-trgo
-wget -O /usr/bin/cek-trgo https://raw.githubusercontent.com/Dimas1441/ipvps/main/cek-trgo.sh && dos2unix /usr/bin/cek-trgo && chmod +x /usr/bin/cek-trgo
-wget -O /usr/bin/xp-trgo https://raw.githubusercontent.com/Dimas1441/error/main/xp-trgo.sh && chmod +x /usr/bin/xp-trgo
-echo "0 0 * * * root xp-trgo" >> /etc/crontab
-echo "DONE INSTALL TROJAN-GO" 
-cd
 rm -f trgo.sh
 mv /root/domain /etc/v2ray
