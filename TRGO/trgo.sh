eab6f65ace3fdd05b0dd5db62b3a98602b428abd
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-02-15 20:29:26 +0800

    Update trgo.sh

--- TRGO/trgo.sh
+++ TRGO/trgo.sh
@@ -17,7 +17,7 @@ touch /etc/trojan-go/uuid.txt
 mkdir -p /etc/trojan-go/
 chmod 777 /etc/trojan-go/
 touch /etc/trojan-go/trojan-go.pid
-wget -O /etc/trojan-go/trojan-go https://github.com/bokir-tampan/ranjau-darate/raw/main/trojan-go
+wget -O /etc/trojan-go/trojan-go https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/TRGO/trojan-go
 chmod +x /etc/trojan-go/trojan-go
 cat <<EOF > /etc/trojan-go/config.json
 {
