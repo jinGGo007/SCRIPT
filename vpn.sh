27ef8422ce3d64a58a859b993200e190313c3b28
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-02-26 06:48:20 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -9,37 +9,20 @@ MYIP=$(wget -qO- icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 ANU=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 
-# Install OpenVPN
-apt install -y openvpn
-wget -q "https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/EasyRSA-3.0.8.tgz"
-tar xvf EasyRSA-3.0.8.tgz
-rm EasyRSA-3.0.8.tgz
-mv EasyRSA-3.0.8 /etc/openvpn/server/easy-rsa
-cp /etc/openvpn/server/easy-rsa/vars.example /etc/openvpn/server/easy-rsa/vars
-sed -i 's/#set_var EASYRSA_REQ_COUNTRY\t"US"/set_var EASYRSA_REQ_COUNTRY\t"MY"/g' /etc/openvpn/server/easy-rsa/vars
-sed -i 's/#set_var EASYRSA_REQ_PROVINCE\t"California"/set_var EASYRSA_REQ_PROVINCE\t"WP"/g' /etc/openvpn/server/easy-rsa/vars
-sed -i 's/#set_var EASYRSA_REQ_CITY\t"San Francisco"/set_var EASYRSA_REQ_CITY\t"Kuala Lumpur"/g' /etc/openvpn/server/easy-rsa/vars
-sed -i 's/#set_var EASYRSA_REQ_ORG\t"Copyleft Certificate Co"/set_var EASYRSA_REQ_ORG\t"JINGGO VPN"/g' /etc/openvpn/server/easy-rsa/vars
-sed -i 's/#set_var EASYRSA_REQ_EMAIL\t"me@example.net"/set_var EASYRSA_REQ_EMAIL\t"mr.sakamalaya@gmail.com"/g' /etc/openvpn/server/easy-rsa/vars
-sed -i 's/#set_var EASYRSA_REQ_OU\t\t"My Organizational Unit"/set_var EASYRSA_REQ_OU\t\t"JINGGO VPN Server"/g' /etc/openvpn/server/easy-rsa/vars
-sed -i 's/#set_var EASYRSA_CA_EXPIRE\t3650/set_var EASYRSA_CA_EXPIRE\t3650/g' /etc/openvpn/server/easy-rsa/vars
-sed -i 's/#set_var EASYRSA_CERT_EXPIRE\t825/set_var EASYRSA_CERT_EXPIRE\t3650/g' /etc/openvpn/server/easy-rsa/vars
-sed -i 's/#set_var EASYRSA_REQ_CN\t\t"ChangeMe"/set_var EASYRSA_REQ_CN\t\t"JINGGO VPN"/g' /etc/openvpn/server/easy-rsa/vars
-cd /etc/openvpn/server/easy-rsa
-./easyrsa --batch init-pki
-./easyrsa --batch build-ca nopass
-./easyrsa gen-dh
-./easyrsa build-server-full server nopass
-cd
-mkdir /etc/openvpn/server/key
-cp /etc/openvpn/server/easy-rsa/pki/issued/server.crt /etc/openvpn/server/key
-cp /etc/openvpn/server/easy-rsa/pki/ca.crt /etc/openvpn/server/key
-cp /etc/openvpn/server/easy-rsa/pki/dh.pem /etc/openvpn/server/key
-cp /etc/openvpn/server/easy-rsa/private/server.key /etc/openvpn/server/key
-wget -qO /etc/openvpn/server/server-tcp-1194.conf "https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/server-tcp-1194.conf"
-wget -qO /etc/openvpn/server/server-udp-2200.conf "https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/server-udp-2200.conf"
-
+# Install OpenVPN dan Easy-RSA
+apt-get update && apt-get install openvpn -y
+apt install easy-rsa unzip -y
+apt install openssl iptables iptables-persistent -y
+mkdir -p /etc/openvpn/server/easy-rsa/
+cd /etc/openvpn/
+wget https://raw.githubusercontent.com/jinGGo007/SCRIPT/main/vpn.zip
+unzip vpn.zip
+rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
+
+cd
+mkdir -p /usr/lib/openvpn/
+cp /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so /usr/lib/openvpn/openvpn-plugin-auth-pam.so
 
 # nano /etc/default/openvpn
 sed -i 's/#AUTOSTART="all"/AUTOSTART="all"/g' /etc/default/openvpn
